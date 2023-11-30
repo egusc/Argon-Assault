b524915a29ac2249cf6e24fec8f3174bcb3461af
Ewan Guscott <ewanguscott@hotmail.co.uk>
2023-11-30 20:42:44 +0000

    Timeline and new logic

--- Argon Assault/Assets/Scripts/PlayerController.cs
+++ Argon Assault/Assets/Scripts/PlayerController.cs
@@ -6,25 +6,54 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 5f;
-    [SerializeField] float xRange = 5f;
+    [SerializeField] float xRange = 3f;
     [SerializeField] float yRange = 2f;
+    [SerializeField] float positionPitchFactor = 2f;
+    [SerializeField] float controlPitchFactor = 10f;
+
+    [SerializeField] float positionRollFactor = 2f;
+    [SerializeField] float controlRollFactor = 30f;
+
+    [SerializeField] float positionYawFactor = 12f;
+    [SerializeField] float controlYawFactor = 20f;
+
+
+    float horizontalMovement, verticalMovement;
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal");
-        float verticalMovement = Input.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
 
         float xOffset = (horizontalMovement * movementSpeed) * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffset;
-        float newXPos = Mathf.Clamp(rawXPos,-xRange, xRange);
+        float newXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
         float yOffset = (verticalMovement * movementSpeed) * Time.deltaTime;
         float rawYPos = transform.localPosition.y + yOffset;
-        float newYPos = Mathf.Clamp(rawYPos,-yRange, yRange);
+        float newYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
         transform.localPosition = new Vector3(
-        newXPos, 
+        newXPos,
         newYPos,
         transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+
+        float pitch = transform.localPosition.y * positionPitchFactor + verticalMovement * controlPitchFactor;
+        float roll = transform.localPosition.z * positionRollFactor + horizontalMovement * controlRollFactor;
+        float yaw = -180f + transform.localPosition.x * positionYawFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
