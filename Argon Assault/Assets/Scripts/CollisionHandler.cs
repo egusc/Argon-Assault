d1dd920a4a324496a5e8a5c459cfc924ef783440
Ewan Guscott <ewanguscott@hotmail.co.uk>
2023-12-01 22:26:34 +0000

    Added collision to player and lasers

--- Argon Assault/Assets/Scripts/CollisionHandler.cs
+++ Argon Assault/Assets/Scripts/CollisionHandler.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    PlayerController playerController;
+
+    private void Start() {
+        playerController = this.GetComponent<PlayerController>();
+    }
+    
     private void OnCollisionEnter(Collision other) {
-        Debug.Log(this.name + " collided with " + other.gameObject.name);
+        playerController.enabled = false;
+        Invoke("ReloadLevel", 1);
+
     }
     private void OnTriggerEnter(Collider other) {
-        Debug.Log($"{this.name} triggered by {other.gameObject.name}");
+        playerController.enabled = false;
+        Invoke("ReloadLevel", 1);
     }
+
+    private void ReloadLevel()
+    {
+        playerController.enabled = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 }
